393bdfdbba4f7b4567d543663a821b6c3775b17a
kenta1114 <61451524+kenta1114@users.noreply.github.com>
2025-07-25 11:51:34 +0900

    Update TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -10,29 +10,31 @@ interface TodoItemProps{
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete })=>{
   return(
-    <Card
-      variant="outlined"
-      sx={{
-        display:'flex',
-        alignItems:'center',
-        padding:'10px',
-        borderRadius:'8px',
-        boxshadow:'none',
-        marginBottom:'8px',
-        borderColor:'#e0e0e0',
-      }}  
-    >
-      <Checkbox color="default"/>
-      <CardContent sx={{padding:'0',flexGrow:1}}>
-        <Typography variant="body1" sx={{fontFamily:'Arial',color:'#333'}}>
-          {todo.text}
-        </Typography>
-      </CardContent>
-    </Card>
-     <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
-      <span>{todo.text}（{todo.category}）</span>
-      <button onClick={() => onDelete(todo.id)}>削除</button>
-    </div>
+    <>
+      <Card
+        variant="outlined"
+        sx={{
+          display:'flex',
+          alignItems:'center',
+          padding:'10px',
+          borderRadius:'8px',
+          boxshadow:'none',
+          marginBottom:'8px',
+          borderColor:'#e0e0e0',
+        }}  
+      >
+        <Checkbox color="default"/>
+        <CardContent sx={{padding:'0',flexGrow:1}}>
+          <Typography variant="body1" sx={{fontFamily:'Arial',color:'#333'}}>
+            {todo.text}
+          </Typography>
+        </CardContent>
+      </Card>
+       <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
+        <span>{todo.text}（{todo.category}）</span>
+        <button onClick={() => onDelete(todo.id)}>削除</button>
+      </div>
+    </>
   );
 };
 
